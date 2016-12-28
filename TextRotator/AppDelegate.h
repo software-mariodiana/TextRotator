81ec367d1af61366f5e44fb0526c299de11d9422
Mario Diana <software@mariodiana.com>
2016-12-27 20:07:24 -0500

    Initial Commit

---
+++ TextRotator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TextRotator
+//
+//  Created by Mario Diana on 12/27/16.
+//  Copyright © 2016 Mario Diana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TextRotator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TextRotator
+//
+//  Created by Mario Diana on 12/27/16.
+//  Copyright © 2016 Mario Diana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
